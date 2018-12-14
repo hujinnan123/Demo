ccaf37099d92cf702aa0869bf973713b48ef71d7
hujinnan123 <1397104290@qq.com>
2018-12-14 23:16:08 +0800

    新建模块

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          我是头部
         </header>
+
+        <content>
+        我是中间
+        </content>
+
+        <footer>
+        我是尾部
+        </footer>
       </div>
     );
   }
