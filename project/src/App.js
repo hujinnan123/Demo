d896eb1d23cac2b732968777faa335e608c2734a
hujinnan123 <1397104290@qq.com>
2018-12-15 14:22:37 +0800

    添加sass

--- project/src/App.js
+++ project/src/App.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component ,Fragment} from 'react';
+import {HashRouter as Router,Route,Link,NavLink,Switch} from "react-router-dom";
+
+import "./reset.css";
+import "./app.scss";
+
+import Home from "./components/footer/components/home"
+import Order from "./components/footer/components/order"
+import My from "./components/footer/components/my"
+import List from "./components/footer/components/list"
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+    <Router>
+      <Fragment>
+        <Switch> {/* 精准匹配 */}
+          <Route path="/home" component = {Home}/>
+          <Route path="/order" component = {Order}/>
+          <Route path="/my" component = {My}/>
+          <Route path="/list" component = {List}/>
+        </Switch>
         <header>
-          我是头部
+          
         </header>
 
         <content>
-        我是中间
+        
         </content>
 
         <footer>
-        我是尾部
+            <ul>
+              <li><NavLink to="/home">首页</NavLink></li>
+              <li><NavLink to="/list">详情</NavLink></li>
+              <li><NavLink to="/order">订阅</NavLink></li>
+              <li><NavLink to="/my">我的</NavLink></li>
+            </ul>
         </footer>
-      </div>
+      </Fragment>
+      </Router>
     );
   }
 }
