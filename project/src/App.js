8aa8c05cbc1ae523ea770b46ed551b850db7f35c
hujinnan123 <1397104290@qq.com>
2018-12-18 23:07:43 +0800

    添加不老少呢

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component ,Fragment} from 'react';
-import {HashRouter as Router,Route,Link,NavLink,Switch} from "react-router-dom";
+import {HashRouter as Router,Route,NavLink,Switch} from "react-router-dom";
 
 import "./reset.css";
 import "./app.scss";
 
-import Home from "./components/footer/components/home"
-import Order from "./components/footer/components/order"
-import My from "./components/footer/components/my"
-import List from "./components/footer/components/list"
+import Home from "./components/home";
+import Order from "./components/order";
+import My from "./components/my";
+import List from "./components/list";
+// console.log(Home);
+
+
 
 
 class App extends Component {
@@ -20,15 +23,9 @@ class App extends Component {
           <Route path="/order" component = {Order}/>
           <Route path="/my" component = {My}/>
           <Route path="/list" component = {List}/>
+          {/* <Route path="/" to = "/home/"/> */}
+
         </Switch>
-        <header>
-          
-        </header>
-
-        <content>
-        
-        </content>
-
         <footer>
             <ul>
               <li><NavLink to="/home">首页</NavLink></li>
